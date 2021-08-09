9bbd9dbc2789d0995b695fc8ed8f0a4f074d0790
YomnaTarek <yomnaragab13@gmail.com>
2021-08-09 21:20:42 +0200

    Added JWT Tutorial

---
+++ JWT Tutorial/MemberJWTDemo/IJwtAuth.cs
@@ -0,0 +1,7 @@
+﻿namespace MemberJWTDemo
+{
+    public interface IJwtAuth
+    {
+        string Authentication(string username, string password);
+    }
+}
---
+++ JWT Tutorial/MemberJWTDemo/Member.cs
@@ -0,0 +1,8 @@
+﻿namespace MemberJWTDemo
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
